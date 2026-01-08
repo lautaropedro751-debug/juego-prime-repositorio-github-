923c11296c927ad0a3b0574d88d7014dc50cd245
lautaropedro751 <lautaropedro751@gmail.com>
2026-01-07 23:36:03 -0300

    Added Idle and Run states

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,22 @@
 using System;
 using System.Diagnostics;
+using UnityEditor.Rendering;
 using UnityEditor.Tilemaps;
 using UnityEngine;
 
 [DebuggerDisplay("{" + nameof(DebuggerDisplayAttribute) + "(),nq}")]
 public class Player : MonoBehaviour
 {
+    //COMPONENTS
     private Rigidbody2D m_rigidbody2D;
     private GatherInput m_gatherInput;
     private Transform m_transform;
+    private Animator m_animator;
+
+    //VALUES
     [SerializeField] private float speed;
     private int direction = 1;
+    private int idspeed;
 
 	// Start is called once before the first execution of Update after the MonoBehaviour is created
 	void Start()
@@ -18,7 +24,19 @@ public class Player : MonoBehaviour
         m_gatherInput = GetComponent<GatherInput>();
         m_transform = GetComponent<Transform>();
         m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_animator = GetComponent<Animator>();
+        idspeed = Animator.StringToHash("speed");
     }
+
+    private void Update()
+    {
+        SetAnimatorValues();
+    }
+
+    private void SetAnimatorValues()
+    {
+		m_animator.SetFloat(idspeed, Mathf.Abs(m_rigidbody2D.linearVelocityX));
+	}
 
     // Update is called once per frame
     void FixedUpdate()  
